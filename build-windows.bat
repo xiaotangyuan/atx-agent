ec5360788ad61970e15105ec7cd991e20673723f
luoyt <xtylove@foxmail.com>
2020-04-07 19:38:44 +0800

    go generate

--- build-windows.bat
+++ build-windows.bat
@@ -3,6 +3,7 @@
 set GOOS=linux
 set GOARCH=arm64
 set DEST="/data/local/tmp/atx-agent"
+go generate || echo ERROR && exit /b
 go build . || echo ERROR && exit /b
 adb push atx-agent %DEST% || echo ERROR && exit /b
 adb forward tcp:7912 tcp:7912 || echo ERROR && exit /b
